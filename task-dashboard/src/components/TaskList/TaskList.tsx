6c72b8fb03014dee83a9533373c3ee467dcc2fec
ShymeetriaJ <sjeducates2@gmail.com>
2025-12-12 11:03:21 -0500

    updated tasklist component with sorted tasks function.

--- task-dashboard/src/components/TaskList/TaskList.tsx
+++ task-dashboard/src/components/TaskList/TaskList.tsx
@@ -1,5 +1,19 @@
 import  { useState } from 'react';
+import { sortByStatus } from '../../utils/taskUtils';
 
 export function TaskList({tasks, onStatusChange, onDelete}: TaskListProps) {
-    const [sortBy, setSortBy] = useState<'priority' | 'status'>()
-}
+    const [sortBy, setSortBy] = useState<'priority' | 'status'>('priority');
+
+    const getSortedTasks = (): Task[] => {
+        if (sortBy === 'priority') {
+            return sortByPriority(tasks);
+        } else {
+            return sortByStatus(tasks);} 
+       };
+
+       const sortedTasks = getSortedTasks();
+
+       return (
+        
+       )
+    }
