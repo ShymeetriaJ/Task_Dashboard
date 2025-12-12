8ea01492ee250cbbdd21c058f0f9006d2d0e41fe
ShymeetriaJ <sjeducates2@gmail.com>
2025-12-12 12:58:46 -0500

    updated task list component return.

--- task-dashboard/src/components/TaskList/TaskList.tsx
+++ task-dashboard/src/components/TaskList/TaskList.tsx
@@ -14,6 +14,33 @@ export function TaskList({tasks, onStatusChange, onDelete}: TaskListProps) {
        const sortedTasks = getSortedTasks();
 
        return (
-        
-       )
+        <div>
+            <h3> Task List </h3>
+            <div>
+                <label> Sort by: </label>
+                <select value={sortBy} onChange={(e) => setSortBy(e.target.value as 'priority' | 'status')}>
+                    <option value="priority">Priority</option>
+                    <option value="status">Status</option>                    
+                </select>
+            </div>
+            {sortedTasks.map((task) => (
+                <li key={task.id}>
+                    <h4>{task.title}</h4>
+                    <p>{task.description}</p>
+                    <p>Priority: {task.priority}</p>
+                    <p>Status: {task.status}</p>
+                    <div>
+                        <label>Change Status:</label>
+                        <select value={task.staus}onChange={(e) => onStatusChange(task.id, e.target.value as 'pending' | 'in-progress' | 'completed')}>
+                            <option value="pending">Pending</option>
+                            <option value="in-progress">In-progress</option>
+                            <option value="completed">Completed</option>
+                        </select>
+                    </div>
+                    <button onClick={() => onDelete(task.id)}>Delete</button>
+                </li>
+            ))}
+        </div>
+
+       );
     }
