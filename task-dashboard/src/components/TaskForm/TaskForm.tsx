d4405b1488eaed1f125b4ea429ae6cfc1b28c9d1
ShymeetriaJ <sjeducates2@gmail.com>
2025-12-12 00:14:22 -0500

    Configured function to add atask.

--- task-dashboard/src/components/TaskForm/TaskForm.tsx
+++ task-dashboard/src/components/TaskForm/TaskForm.tsx
@@ -1,4 +1,5 @@
 import { useState} from "react";
+import { validateDueDate, validateTitle } from "../../utils/taskUtils";
 
 export function TaskForm({ onSubmit }: TaskFormProps) {
   const [title, setTitle] = useState('');
@@ -6,6 +7,14 @@ export function TaskForm({ onSubmit }: TaskFormProps) {
 
   const handleSubmit = (e:React.FormEevent) => {
     e.preventDefault();
+
+    const titleError = !validateTitle(title) ? 'Title required' : '';
+    const dateError = !validateDueDate(dueDate) ? 'Date required' : '';
+
+    if (titleError || dateError) {
+        setErrors({ title: titleError, dueDate: dateError});
+        return;
+    }
   }
 
 }
